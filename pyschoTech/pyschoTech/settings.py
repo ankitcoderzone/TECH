f92685c0028ba0cf02881e0e5f458875c24b8fda
ankitcoderzone <ankityadav559008@gmail.com>
2025-02-14 19:39:54 +0530

    3rd

--- pyschoTech/pyschoTech/settings.py
+++ pyschoTech/pyschoTech/settings.py
@@ -38,7 +38,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tailwind',
+    'theme',
+    'pycotech',
+    'django_browser_reload'
 ]
+
+TAILWIND_APP_NAME='theme'
+
+INTERNAL_IPS =[
+    "127.0.0.1",
+]
+
+NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -48,6 +60,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'pyschoTech.urls'
